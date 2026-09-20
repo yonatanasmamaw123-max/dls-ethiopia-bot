cc69f85ddedd99bff99d15fa18b46c9e1d3cdc3c
yonatanasmamaw123-max <yonatanasmamaw1.2.3@gmail.com>
2026-09-20 03:08:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 
+import os
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+# Render ፖርት እንዲያገኝ የሚረዳ ቀላል ሰርቨር
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running!")
+
+def run_dummy_server():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(('0.0.0.0', port), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+# ሰርቨሩን በ Background ማነሳት
+threading.Thread(target=run_dummy_server, daemon=True).start()
